f5c7df50ee2aef40398d63894ab6d1cb3c0ed211
0sant1ago <109995932+0sant1ago@users.noreply.github.com>
2023-05-15 16:59:20 -0400

    Update indices.sql

--- indices.sql
+++ indices.sql
@@ -1,14 +1,29 @@
 --AUTHOR table:
 CREATE INDEX ix_author_author_name ON author (author_name);
 
+--Since the author's name is frequently used in queries, indexing the 
+-- AUTHOR_NAME field can speed up searches and sorting based on the author's name.
+
+
 --CATEGORY table:
 CREATE INDEX ix_category_category_name ON category (category_name);
+
+--indexing the CATEGORY_NAME field can improve the performance of queries that 
+-- involve filtering or sorting based on category names.
+
 
 --BOOK table:
 CREATE INDEX ix_book_category_id ON book (category_id);
 
+--creating an index on this field can improve the efficiency of queries that 
+-- involve retrieving books by category
+
+
 --EDITORIAL table:
 CREATE INDEX ix_editorial_editorial_name ON editorial (editorial_name);
+
+--creating an index on the EDITORIAL_NAME field can enhance query performance
+
 
 --BOOK_COPY table:
 CREATE INDEX ix_editorial_editorial_name ON editorial (editorial_name);
